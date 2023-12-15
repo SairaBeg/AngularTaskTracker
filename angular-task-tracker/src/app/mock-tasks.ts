2c782c5d442c28be98e503a6384d6e2a9592e9a1
SairaBeg <sairabeg.dev@gmail.com>
2023-12-14 22:01:53 -0500

    Task model + component made

---
+++ angular-task-tracker/src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Grocery Shopping',
+    day: 'May 6th at 9:00am',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Interview',
+    day: 'December 14th at 3:00pm',
+    reminder: true,
+  },
+];
